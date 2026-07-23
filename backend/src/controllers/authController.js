b2b9892a5a7ca8019679fcc29d6026086ebd7210
cyrilfoucher <macylcyril@hotmail.fr>
2026-07-23 21:44:54 +0200

    corection authController->register

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,24 +1,18 @@
-import bcrypt from bcrypt;
-import transporter from"./config/mailer.js";
-export const registrer =asyncHandler (async (req,res)=>{
-    const {nom,prenom,email,motDePasse,confirmationMotDePasse}=req.body;
-    const utilisateurExiste= await Utilisateur.findOne({email});
-    if(utilisateurExiste){
-        return (
-            res
-            .status(409)
-            .json({message:"Email déjà utilisé"})
-        );
-const motdePasseHash=bcrypt.hash(motDePasse,10)
-await Utilisateur.create({nom,prenom,email,motDepasse:motdePasseHash});
-await Transporter.sendMail({
-    from:ProcessingInstruction.apply.env.EMAIL_USER,
-    to:email,
-    suject:"Création de votre compte utilisateur",
-    text:"Votre compte a été créer avec succés"
-
+import bcrypt from "bcrypt";
+import transporter from "./config/mailer.js";
+export const register = asyncHandler(async (req, res) => {
+  const { nom, prenom, email, motDePasse, confirmationMotDePasse } = req.body;
+  const utilisateurExiste = await Utilisateur.findOne({ email });
+  if (utilisateurExiste) {
+    return res.status(409).json({ message: "Email déjà utilisé" });
+  }
+  const motdePasseHash = await bcrypt.hash(motDePasse, 10);
+  await Utilisateur.create({ nom, prenom, email, motDePasse: motdePasseHash });
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Création de votre compte utilisateur",
+    text: "Votre compte a été créer avec succés",
+  });
+  return res.status(201).json({ message: "Compte créer avec succés" });
 });
-return res
-.status(201)
-.json({message:"Compte créer avec succés"})    }
-})
